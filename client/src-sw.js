dcce452e5a8926f18fd73ac958f32621f771f233
Jacobschoenbauer <jhwt65@gmail.com>
2023-01-24 11:36:10 -0600

    changed src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -24,17 +24,17 @@ warmStrategyCache({
   strategy: pageCache,
 });
 // TODO: Implement asset caching
-registerRoute(({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-new StaleWhileRevalidate({
-  
-  cacheName: 'asset-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-  ],
-})
-); request.mode === 'navigate', pageCache;
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-
-registerRoute();
+// Set up asset cache
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
